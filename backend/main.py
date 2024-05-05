71fc941531563fc18e141cf34b5794c4b5d707a2
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2024-05-05 15:52:04 -0700

    Fixing CORS issues

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ huggingFaceAPI = os.getenv('HUGGING_FACE')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
